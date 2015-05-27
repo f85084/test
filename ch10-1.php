5f78dda6ed4de1b8b4f8e81c58ed33646535eba1
f85084 <f85084@yahoo.com.tw>
2015-05-27 15:44:30 +0800

    查詢-排序-刪除功能 簡化版
    感謝蘇胖提供

--- ch10-1.php
+++ ch10-1.php
@@ -20,80 +20,69 @@
 <?php
 //header('Content-Type: text/html; charset=utf-8');
 include("mydb.php");
-if ($_GET['del'])
-{
+
+// 刪除
+if ($_GET['del']) {
 	$a=$_GET['del'];
 	$d="delete from books where 書籍編號=$a";
 	mysql_query($d);
 	//異動會顯示異動資料
 	echo '成功幾筆'.mysql_affected_rows();
 }
+
 $no=$_GET['no'];
-if (!$_GET['no'])
-$sql="select 書籍編號,書籍名稱,價格,姓名 from books join employee on  負責員工編號=員工編號";
-else
-{
-$sql="select 書籍編號,書籍名稱,價格,姓名 from books join employee on  負責員工編號=員工編號 where 書籍編號=$no";
+
+// 員工編號查詢
+if (!$_GET['no']) {
+	$sql="select 書籍編號,書籍名稱,價格,姓名 from books join employee on  負責員工編號=員工編號";
+} else {
+	$sql="select 書籍編號,書籍名稱,價格,姓名 from books join employee on  負責員工編號=員工編號 where 書籍編號=$no";
 }
-//排序
-if ($_GET['order']==1)
-{
+
+// 使用價格排序
+if ($_GET['order']==1) {
 	$sql="select 書籍編號,書籍名稱,價格,姓名 from books join employee on  負責員工編號=員工編號 order by 價格";
-	}
-if ($_GET['order']==2)
-{
+}
+if ($_GET['order']==2) {
 	$sql="select 書籍編號,書籍名稱,價格,姓名 from books join employee on  負責員工編號=員工編號 order by 價格 desc";
-	}
-//$sql = "SELECT * FROM books";
+}
+
 //回傳結果
 $result=mysql_query($sql);
+
 echo '總共有' .mysql_num_rows($result).'書';
-if (!$_GET['order'])
-{
 echo "<table border=1>";
-echo 
-  "<tr>
-    <td width=10%>書本編號</td>
-    <td width=40%>書本名稱</td>
-    <td width=20%><a href=ch10-1.php?order=1>書本價格</a></td>
-    <td width=10%>負責員工</td>
-	<td width=10%>刪除</td>
-  </tr>";
+
+if ($_GET['order']==2) {
+	echo 
+		"<tr>
+			<td width=10%>書本編號</td>
+			<td width=40%>書本名稱</td>
+			<td width=20%><a href=ch10-1.php?order=1>書本價格</a></td>
+			<td width=10%>負責員工</td>
+			<td width=10%>刪除</td>
+		</tr>";
+} else {
+	echo 
+		"<tr>
+			<td width=10%>書本編號</td>
+			<td width=40%>書本名稱</td>
+			<td width=20%><a href=ch10-1.php?order=2>書本價格</a></td>
+			<td width=10%>負責員工</td>
+			<td width=10%>刪除</td>
+		</tr>";
 }
-if ($_GET['order']==2)
-{
-echo "<table border=1>";
-echo 
-  "<tr>
-    <td width=10%>書本編號</td>
-    <td width=40%>書本名稱</td>
-    <td width=20%><a href=ch10-1.php?order=1>書本價格</a></td>
-    <td width=10%>負責員工</td>
-	<td width=10%>刪除</td>
-  </tr>";
+
+while ($row=mysql_fetch_array($result)) {
+	echo 
+		"<tr>
+			<td width=10%>$row[0]</td>
+			<td width=40%>$row[1]</td>
+			<td width=20%>$row[2]</td>
+			<td width=10%>$row[3]</td>
+			<td width=10%><a href=ch10-1.php?del=$row[0]>刪除</td>
+		</tr>";
 }
-if ($_GET['order']==1)
-{
-echo "<table border=1>";
-echo 
-  "<tr>
-    <td width=10%>書本編號</td>
-    <td width=40%>書本名稱</td>
-    <td width=20%><a href=ch10-1.php?order=2>書本價格</a></td>
-    <td width=10%>負責員工</td>
-	<td width=10%>刪除</td>
-  </tr>";
-}
-while ($row=mysql_fetch_array($result))
-{
-echo 
-  "<tr>
-    <td width=10%>$row[0]</td>
-    <td width=40%>$row[1]</td>
-    <td width=20%>$row[2]</td>
-    <td width=10%>$row[3]</td>
-	<td width=10%><a href=ch10-1.php?del=$row[0]>刪除</td>
-  </tr>";
-}
+
 echo "</table>";
 ?>
