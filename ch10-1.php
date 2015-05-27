57b516824d6e45f3cdc67477f345d5b55ce436a0
f85084 <f85084@yahoo.com.tw>
2015-05-27 16:53:11 +0800

    查詢-全部排序-刪除 //完整功能  極簡版

--- ch10-1.php
+++ ch10-1.php
@@ -38,13 +38,33 @@ $sql = "select 書籍編號,書籍名稱,價格,姓名 from books join employee 
 if ($_GET['no']) {
 	$sql = $sql."where 書籍編號=".$no;
 }
-
-// 使用價格排序
+//使用編號排序
 if ($_GET['order']==1) {
-	$sql = $sql."order by 價格";
+	$sql = $sql."order by 書籍編號";
 }
 if ($_GET['order']==2) {
+	$sql = $sql."order by 書籍編號 desc";
+}
+//使用編號排序
+if ($_GET['order']==3) {
+	$sql = $sql."order by 書籍名稱";
+}
+if ($_GET['order']==4) {
+	$sql = $sql."order by 書籍名稱 desc";
+}
+// 使用價格排序
+if ($_GET['order']==5) {
+	$sql = $sql."order by 價格";
+}
+if ($_GET['order']==6) {
 	$sql = $sql."order by 價格 desc";
+}
+// 使用價姓名序
+if ($_GET['order']==7) {
+	$sql = $sql."order by 姓名";
+}
+if ($_GET['order']==8) {
+	$sql = $sql."order by 姓名 desc";
 }
 
 //回傳結果
@@ -53,17 +73,35 @@ $result=mysql_query($sql);
 // 表格表題
 echo '總共有' .mysql_num_rows($result).'書';
 echo "<table border=1>
-		<tr>
-			<td width=10%>書本編號</td>
-			<td width=40%>書本名稱</td>";
-
+		<tr>";
 if ($_GET['order']==2) {
-	echo "	<td width=20%><a href=ch10-1.php?order=1>書本價格</a></td>";
-} else {
-	echo "	<td width=20%><a href=ch10-1.php?order=2>書本價格</a></td>";
+	echo "	<td width=10%><a href=ch10-1.php?order=1>書本編號</a></td>";
+} 
+else 
+{
+	echo "	<td width=10%><a href=ch10-1.php?order=2>書本編號</a></td>";
 }
 
-echo "		<td width=10%>負責員工</td>
+if ($_GET['order']==4) {
+	echo "	<td width=10%><a href=ch10-1.php?order=3>書本名稱</a></td>";
+} 
+else 
+{
+	echo "	<td width=10%><a href=ch10-1.php?order=4>書本名稱</a></td>";
+}
+
+if ($_GET['order']==6) {
+	echo "	<td width=20%><a href=ch10-1.php?order=5>書本價格</a></td>";
+} else {
+	echo "	<td width=20%><a href=ch10-1.php?order=6>書本價格</a></td>";
+}
+if ($_GET['order']==8) {
+	echo "	<td width=10%><a href=ch10-1.php?order=7>負責員工</a></td>";
+} else {
+	echo "	<td width=10%><a href=ch10-1.php?order=8>負責員工</a></td>";
+}
+
+echo "		
 			<td width=10%>刪除</td>
 		</tr>";
 
